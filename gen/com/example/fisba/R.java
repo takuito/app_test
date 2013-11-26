b785c98c8bc63a287d7aae01bfd53d93381ea8d5
takuito <takumi-itou@alpsgiken.co.jp>
2013-11-27 02:05:39 +0900

    更新

--- gen/com/example/fisba/R.java
+++ gen/com/example/fisba/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int scan_button=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int btnSave=0x7f080003;
+        public static final int btnScan=0x7f080001;
+        public static final int btnStop=0x7f080002;
         public static final int text=0x7f080000;
     }
     public static final class layout {
