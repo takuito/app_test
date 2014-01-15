d58f128d18ed894f9267583dfb59f4afcf20d090
takuito <takumi-itou@alpsgiken.co.jp>
2014-01-16 02:25:52 +0900

    指紋編集：指紋名と選択した画像をテキストに保存

--- src/com/example/fisba/FingerprintEdit.java
+++ src/com/example/fisba/FingerprintEdit.java
@@ -3,9 +3,11 @@ package com.example.fisba;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import android.app.Activity;
@@ -34,6 +36,9 @@ public class FingerprintEdit extends Activity {
 	public final static int ID_IMAGE_GALLERY = 1;
 	public final static int ID_IMAGE_CROP  = 2;
 	private static final int REQUEST_GALLERY = 0;
+	
+	private int click_num;
+	private String setFilename;
 	
 	//private static String[] str_Name;
 	
@@ -96,7 +101,18 @@ public class FingerprintEdit extends Activity {
                         @Override  
                         public void onItemClick(AdapterView<?> parent,  
                                 View view, int position, long id) {  
-                            // クリックされた時の処理  
+                            // クリックされた時の処理
+                        	//mScannerInfo2.setText("猫");
+                        	
+                        	
+                        	
+                        	Intent intent = new Intent();
+                        	intent.setType("image/*");
+                        	intent.setAction(Intent.ACTION_PICK);
+                        	intent = Intent.createChooser(intent,  "Select Gallery App");
+                        	startActivityForResult(intent, ID_IMAGE_GALLERY);
+                        	
+                        	click_num = position;
                         }  
                       });  
                   } else {
@@ -109,7 +125,7 @@ public class FingerprintEdit extends Activity {
                 }
             }
         });
-    	
+    	        
         /*
     	mButtonEditStart.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -145,9 +161,75 @@ public class FingerprintEdit extends Activity {
             	finish();
             }
         });
-      }  
-        
+      }
     
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode) {
+         case ID_IMAGE_GALLERY:
+			 if (resultCode == Activity.RESULT_OK) {
+				 //mScannerInfo2.setText("Ca");
+				 
+				 Uri uri = data.getData();
+				 mScannerInfo2.setText(uri.toString());
+				 
+				 BufferedReader in = null;
+				 try {
+					 FileInputStream fileRead = openFileInput("test.txt");
+	                  in = new BufferedReader(new InputStreamReader(fileRead));
+	                  String str = in.readLine();
+	                  String[] str_Name = str.split(",", 0);
+	                  //Toast.makeText(FingerprintEdit.this, String.format("%d", str_Name.length),Toast.LENGTH_SHORT).show();
+	                  if( (str_Name.length % 3) == 0 ){
+	                		  setFilename = str_Name[click_num*3+1];
+	                	  }
+	                  in.close();
+			        } catch (IOException e) {
+			            e.printStackTrace();
+			        }
+				 
+				 try {
+		        		// ストリームを開く
+		        		FileOutputStream outStream = openFileOutput("hensyu.txt", MODE_APPEND);
+		        		OutputStreamWriter writer = new OutputStreamWriter(outStream);
+		        		writer.write(setFilename + "," + uri + ",");
+		        		writer.flush();
+		        		writer.close();
+			        } catch (IOException e) {
+			            e.printStackTrace();
+			        }
+				 
+				 /*
+				 try {  
+				      // 一時ファイル場所のUri取得処理  
+				      Uri mUri = data.getData();  
+				      if (mUri == null) {  
+				       String action = data.getAction();  
+				       if (action != null && action.indexOf("content://") > -1) {  
+				        mUri = Uri.parse(action);  
+				       }
+				       
+				      }
+				      mScannerInfo2.setText(mUri.toString());
+				      
+				 } catch (Exception e) {  
+				      Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();  
+				      e.printStackTrace();  
+				  }
+				  */
+				 
+				 
+			     // Get the file format
+				 //String[] extraString = data.getExtras().getStringArray(SelectFileFormatActivity.EXTRA_FILE_FORMAT);
+				 //String fileFormat = extraString[0];
+				 //String fileName = extraString[1];
+				 //SaveImageByFileFormat(fileFormat, fileName);
+             }
+			 else
+				 mScannerInfo2.setText("Cancelled!");
+             break;            
+        }
+    }
+
       class data{
     	  Integer num;
     	  String name;
